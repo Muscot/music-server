55ddc0b7c3fb9ea280bec31fc338d8d81b627529
Martin Palmér <martin.palmer.develop@gmail.com>
2016-08-01 20:17:44 +0200

    added connect-timeout to express

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 var SwaggerExpress = require('swagger-express-mw');
 var app = require('express')();
+var timeout = require('connect-timeout');
 module.exports = app; // for testing
 
 var config = {
@@ -11,10 +12,20 @@ var config = {
 SwaggerExpress.create(config, function(err, swaggerExpress) {
   if (err) { throw err; }
 
+  app.use(timeout(10 * 60 * 1000)); // 10 minute
+
   // install middleware
   swaggerExpress.register(app);
 
   var port = process.env.PORT || 10010;
   app.listen(port);
+  app.use((req, res, next) => {
+    if (!req.timedout) next();
 
+    res.set('Content-Type', 'application/json');
+    res.send({
+        'status' : 500,
+        'message' : "my timeout"
+    });
+  });
 });
