bfae9fd829c3467cbddc3072e89437b16bc62e21
Martin Palmér <martin.palmer.develop@gmail.com>
2016-08-01 20:33:30 +0200

    better throttle and retry for musicbrainz

--- loaders/musicbrainz.js
+++ loaders/musicbrainz.js
@@ -1,9 +1,19 @@
 import * as rp from 'request-promise';
 import {reduce} from 'lodash';
+//import * as Promise from 'bluebird';
+//import * as PromiseThrottle from 'promise-throttle';
+
+var PromiseThrottle = require('promise-throttle');
+var Promise = require('bluebird');
+var retry = require('bluebird-retry');
+
+var promiseThrottle = new PromiseThrottle({
+    requestsPerSecond: 1,           // up to 10 requests per second
+    promiseImplementation: Promise  // the Promise library you are using
+});
 
 var restApi = 'http://musicbrainz.org/ws/2';
 var userAgent = 'Amivono/0.0.1 ( martin@amivono.com )';
-
 
 /**
  * Get all the musicbrainz relations with relation type as keys.
@@ -52,6 +62,43 @@ function _getAlbums(json)
     });
 }
 
+function _getRequest(mbid)
+{
+    console.log("get from musicbrainz:" + mbid);
+    var restUrl = `${restApi}/artist/${mbid}?&fmt=json&inc=url-rels+release-groups`;
+    var options = {
+        uri: restUrl,
+        headers: {
+            'User-Agent': userAgent
+        },
+        json: true
+    };
+
+    return rp.get(options).then((json) => {
+        var ret = {
+            mbid : json.id,
+            albums : _getAlbums(json),
+            sources : {
+                'musicbrainz' : {
+                    'id' : json.id,
+                    'url' : restUrl
+                }
+            } 
+        };
+
+        // add wikipedia if it exists;
+        var relations = _getRelations(json);
+        if (relations.wikipedia && relations.wikipedia.url)
+            ret.sources.musicbrainz['wikipedia'] = relations.wikipedia.url.resource;
+
+        return ret;
+    }).catch((err) => {
+        console.log(err.statusCode);
+        throw err;
+    });
+};
+
+
 
 /**
  * Get information from musicbrainz this will be added to the structure of our API see swagger
@@ -63,31 +110,14 @@ function _getAlbums(json)
  */
 export function get(mbid)
 {
-    var restUrl = `${restApi}/artist/${mbid}?&fmt=json&inc=url-rels+release-groups`;
-    var options = {
-        uri: restUrl,
-        headers: {
-            'User-Agent': userAgent
-        },
-        json: true
+    var options = 
+    {
+        backoff : 2,
+        timeout : 1 * 60 * 1000, // 1 minute timeout for all request;
+        max_tries : 10
     };
 
-    return rp.get(options).then((json) => {
-        var relations = _getRelations(json);
-        var ret = {
-            mbid : json.id,
-            albums : _getAlbums(json),
-            sources : {
-                'musicbrainz' : {
-                    'id' : json.id,
-                    'url' : restUrl,
-                    'relations' : {
-                        'wikipedia' : relations.wikipedia
-                    }
-                },
-            } 
-        };
-        console.log(relations.wikipedia.url.resource);
-        return ret;
-    });
+    return retry(() => {
+        return promiseThrottle.add(_getRequest.bind(this, mbid));
+    }, options);
 }
--- test/loaders/musicbrainz_spec.js
+++ test/loaders/musicbrainz_spec.js
@@ -1,5 +1,7 @@
-import * as musicbrain from '../../loaders/musicbrainz';
+import * as musicbrainz from '../../loaders/musicbrainz';
 import {expect} from 'chai';
+var Promise = require('bluebird');
+
 describe('loaders', function() { 
 
   describe('musicbrainz', function() {
@@ -8,11 +10,10 @@ describe('loaders', function() {
 
       it('should return a data from musicbrainz', (done) => {
         
-        musicbrain.get('5b11f4ce-a62d-471e-81fc-a69a8278c7da').then((result) => 
+        musicbrainz.get('5b11f4ce-a62d-471e-81fc-a69a8278c7da').then((result) => 
         {
             expect(result).to.have.all.keys('mbid', 'albums', 'sources');
             expect(result.mbid).to.be.a('string');
-            console.log(result);
             done();
         }).catch((err) =>
         {
@@ -22,6 +23,33 @@ describe('loaders', function() {
 
       });
 
+      it('should return data for all musicbrainz ids (throttle test)', (done) => {
+
+        done();
+        return;
+        var ids = [
+          '5eecaf18-02ec-47af-a4f2-7831db373419',
+          'ff6e677f-91dd-4986-a174-8db0474b1799',
+          '7249b899-8db8-43e7-9e6e-22f1e736024e',
+          '5b11f4ce-a62d-471e-81fc-a69a8278c7da',
+          'e1f1e33e-2e4c-4d43-b91b-7064068d3283'
+        ];
+        var requests = [];
+        for (var index = 0; index < ids.length; index++) {
+          let mbid = ids[index];
+          requests.push(musicbrainz.get(mbid).then((result) => 
+          {
+              expect(result).to.have.all.keys('mbid', 'albums', 'sources');
+              expect(result.mbid).to.be.a('string');
+          }).catch((err) =>
+          {
+            console.log(err.message);
+            throw err;
+          }));
+        }
+
+        Promise.all(requests).done(() => done());
+      });
     });
 
   });
