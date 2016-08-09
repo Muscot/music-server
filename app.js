aac573fd3b67c6b935502ca640234d3d73ee9e1c
Martin Palmér <martin.palmer.develop@gmail.com>
2016-08-09 20:40:14 +0200

    app and ws refactor use register middleware

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 'use strict';
-import DebugLib from 'debug';
-
 var ws = require('./socket');
 var express = require('express');
 var SwaggerExpress = require('swagger-express-mw');
 var timeout = require('connect-timeout');
 
-var debug = DebugLib('web');
 var app = express();
 module.exports = app; // for testing
 
@@ -18,16 +15,13 @@ SwaggerExpress.create(config, function(err, swaggerExpress) {
   if (err) { throw err; }
 
   app.use(timeout(10 * 60 * 1000)); // 10 minute
-  app.use(function (req, res, next) {
-    debug(req.method + " " + req.url);
-    next();
-  });
 
   // install middleware
+  ws.register(app);
   swaggerExpress.register(app);
 
   var port = process.env.PORT || 10010;
 
   app.use('/', express.static('public'));
-  ws.createServer(app.listen(port));
+  ws.listen(app.listen(port));
 });
--- socket/index.js
+++ socket/index.js
@@ -1,13 +1,25 @@
 import DebugLib from 'debug';
 import socketIO from 'socket.io';
 import os from 'os';
+import {cache, pending} from '../cache';
 
 var io;
-var stats = {
-  started : new Date().toUTCString()
+var debug = DebugLib('web');
+var counters = {
+  'request' : 0
 };
+var stats = {
+  started : new Date().toUTCString(),
+};
+
+/**
+ * Collect all data to send back to the client throught webSocket.
+ */
 function collectServerStat()
 {
+    stats['cache'] = cache.size;
+    stats['pending'] = pending.size;
+    stats['request'] = counters.request;
     stats['memory'] = process.memoryUsage();
     stats['cpu'] = os.loadavg();
     if (io)
@@ -16,6 +28,12 @@ function collectServerStat()
 setInterval(collectServerStat, 3000);
 
 var sendingLogMessage = false;
+/**
+ * Send log message to client 
+ * 
+ * @export
+ * @param {any} message
+ */
 export function log(message)
 {
     if (io && !sendingLogMessage)
@@ -29,8 +47,33 @@ export function log(message)
     console.log(message);
 }
 
+// override DEBUG log function and use our own to send to client.
 DebugLib.log = log;
-export function createServer(server)
+
+
+/**
+ * register middleware.
+ * 
+ * @export
+ * @param {any} app
+ */
+export function register(app)
+{
+  app.use(function (req, res, next) {
+    debug(req.method + " " + req.url);
+    counters.request++;
+    next();
+  });
+}
+
+/**
+ * 
+ * 
+ * @export
+ * @param {object} server Express Server instance to upgrade protocol to WebSocket.
+ * @returns
+ */
+export function listen(server)
 {
   io = socketIO(server);
 
