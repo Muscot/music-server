0aeb3a0e2976c5c35ee2b5347ba143f86cd3f384
Martin Palmér <martin.palmer.develop@gmail.com>
2016-08-09 19:22:14 +0200

    added DEBUG to web/express

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 'use strict';
+import DebugLib from 'debug';
 
+var ws = require('./socket');
 var express = require('express');
 var SwaggerExpress = require('swagger-express-mw');
 var timeout = require('connect-timeout');
 
+var debug = DebugLib('web');
 var app = express();
 module.exports = app; // for testing
 
@@ -15,6 +18,10 @@ SwaggerExpress.create(config, function(err, swaggerExpress) {
   if (err) { throw err; }
 
   app.use(timeout(10 * 60 * 1000)); // 10 minute
+  app.use(function (req, res, next) {
+    debug(req.method + " " + req.url);
+    next();
+  });
 
   // install middleware
   swaggerExpress.register(app);
@@ -22,5 +29,5 @@ SwaggerExpress.create(config, function(err, swaggerExpress) {
   var port = process.env.PORT || 10010;
 
   app.use('/', express.static('public'));
-  app.listen(port);
+  ws.createServer(app.listen(port));
 });
